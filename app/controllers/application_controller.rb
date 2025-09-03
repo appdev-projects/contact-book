f0c5a36037a3e42da5fe6fe387bc52485039b98d
Ben Purinton <ben.purinton@gmail.com>
2025-09-03 14:49:58 -0700

    Skip forgery protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  skip_forgery_protection
 end
